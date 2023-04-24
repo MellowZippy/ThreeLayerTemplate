5d5bd154307b3f59cf942a736fd0e6d5b7197f8c
MellowZippy <124064665+MellowZippy1@users.noreply.github.com>
2023-04-24 16:02:30 +0200

    Minimalised CustomerLogic and FoodLogic.

--- Project/Logic/CustomerLogic.cs
+++ Project/Logic/CustomerLogic.cs
@@ -1,12 +1,25 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.IO;
+using System.Text.Json;
 
-namespace Project.Logic
+
+//This class is not static so later on we can use inheritance and interfaces
+class CustomerLogic
 {
-    public class CustomerLogic
+    private List<CustomerModel> _customers;
+
+    //Static properties are shared across all instances of the class
+    //This can be used to get the current logged in account from anywhere in the program
+    //private set, so this can only be set by the class itself
+    
+    public CustomerLogic()
     {
-        
+        _customers = CustomerAccess.LoadAll();
     }
+
+    public CustomerModel GetById(int id)
+    {
+        return _customers.Find(i => i.customerId == id)!;
+    }    
 }
